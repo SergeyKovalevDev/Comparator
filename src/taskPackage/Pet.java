13801c54cfef8a0354021dabaa090e02e0481a5b
Sergey Kovalev <dip.deep@mail.ru>
2022-02-05 17:59:05 +0300

    added class Pet.java

--- src/taskPackage/Pet.java
+++ src/taskPackage/Pet.java
@@ -1,5 +1,30 @@
 package taskPackage;
 
 public class Pet {
+	
+	private final String breed;
+	private final String name;
+	private final int weight;
+	
+	public Pet(String breed, String name, int weight) {
+		this.breed = breed;
+		this.name = name;
+		this.weight = weight;
+	}
+
+	public String getBreed() {
+		return breed;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getWeight() {
+		return weight;
+	}
+	
+	
+	
 
 }
