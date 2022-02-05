28ff030de5fea52709f2bc88ec6e32874bebac10
Sergey Kovalev <dip.deep@mail.ru>
2022-02-05 20:54:09 +0300

    modified Main.java

--- src/taskPackage/Main.java
+++ src/taskPackage/Main.java
@@ -16,9 +16,28 @@ public class Main {
 		petSet.add(new Pet("Cat", "Bob", 10));
 		petSet.add(new Pet("Peeg", "John", 100));
 		petSet.add(new Pet("Dog", "Master", 20));
-		for (Pet pet : petSet) {
+		setPrinter(petSet);
+		
+		Set<Pet> petSet2 = new TreeSet<>(new Comparator<Pet>() {
+
+			@Override
+			public int compare(Pet o1, Pet o2) {
+				Integer nameLength1 = o1.getName().length();
+				Integer nameLength2 = o2.getName().length();
+				return nameLength1.compareTo(nameLength2);
+			}
+			
+		});
+		petSet2.addAll(petSet);
+		setPrinter(petSet2);
+	
+	}
+	
+	private static void setPrinter(Set<Pet> set) {
+		for (Pet pet : set) {
 			System.out.println(pet);
 		}
+		
 	}
 
 
