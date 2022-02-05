5f99e0ee873b20acd160ca2ca919963c987d5435
Sergey Kovalev <dip.deep@mail.ru>
2022-02-05 19:22:00 +0300

    comparator moved to anonimous class

--- src/taskPackage/Main.java
+++ src/taskPackage/Main.java
@@ -1,12 +1,18 @@
 package taskPackage;
 
+import java.util.Comparator;
 import java.util.Set;
 import java.util.TreeSet;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Set<Pet> petSet = new TreeSet<>(new weightComparator());
+		Set<Pet> petSet = new TreeSet<>(new Comparator<Pet>() {
+			@Override
+			public int compare(Pet o1, Pet o2) {
+				return o1.getWeight().compareTo(o2.getWeight());
+			}
+		});
 		petSet.add(new Pet("Cat", "Bob", 10));
 		petSet.add(new Pet("Peeg", "John", 100));
 		petSet.add(new Pet("Dog", "Master", 20));
